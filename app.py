b66fe5f2674d562f8c92adcc94beabdfdb6c6dc6
hcconnor <hamiltoncconnor@gmail.com>
2024-06-14 00:30:53 -0700

    input is sending to py script

--- app.py
+++ app.py
@@ -3,14 +3,17 @@ from mtg_set_search import *
 
 app = Flask(__name__)
 
-@app.route('/', methods=['POST'])
-def index():    
-
+@app.route('/', methods=['GET', 'POST'])
+def index():   
     if request.method == 'POST':
-        decklist = request.form.get('deckTextArea')
-        search_database(decklist)
-
+        decklist = request.form.get('deckText')
+        search_database(decklist) 
     return render_template('template.html')
 
-if __name__ == '__main__': 
-    app.run() 
+# @app.route('/deckInput', methods=['POST'])
+# def deckInput():
+#     if request.method == 'POST':
+#         decklist = request.form.get('deckTextArea')
+#         search_database(decklist)
+
+    
